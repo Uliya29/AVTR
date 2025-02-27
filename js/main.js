228e5ee6d0550020ba30b609b6c20346d50dba74
Uliya Zayceva <juliyazayceva@mail.ru>
2025-02-27 15:25:26 +0800

    create adaptive

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ $(function(){
     centerMode: true,
     variableWidth: true,
     prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="Влево">',
-    nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="Вправо">'
+    nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="Вправо">',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 1,
+          centerMode: false,
+          prevArrow: false,
+          nextArrow: false,
+        }
+      }
+    ]
   });
 });
